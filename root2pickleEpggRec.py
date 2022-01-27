ab7a632098bcdf7a71af5ee9a4fad996eb26b75b
Sangbaek <sangbaek@mit.edu>
2022-01-27 09:50:40 -0500

    change indentation level

--- root2pickleEpggRec.py
+++ root2pickleEpggRec.py
@@ -576,7 +576,7 @@ class root2pickle():
         cut_Ge2 = df_dvpi0p["Ge2"] > 0.6  # Ge cut. Ge>3 for DVCS module.
         cut_Esector = (df_dvpi0p["Esector"]!=df_dvpi0p["Gsector"]) & (df_dvpi0p["Esector"]!=df_dvpi0p["Gsector2"])
         cut_Psector = ~( ((df_dvpi0p["Pstat"]//10)%10>0) & (df_dvpi0p["Psector"]==df_dvpi0p["Gsector"]))
-                        & ~( ((df_dvpi0p["Pstat"]//10)%10>0) & df_dvpi0p["Psector"]!=df_dvpi0p["Gsector2"])
+                    & ~( ((df_dvpi0p["Pstat"]//10)%10>0) & df_dvpi0p["Psector"]!=df_dvpi0p["Gsector2"])
         cut_Ppmax = df_dvpi0p.Pp < 0.8  # Pp
         # cut_Vz = np.abs(df_dvcs["Evz"] - df_dvcs["Pvz"]) < 2.5 + 2.5 / mag([df_dvcs["Ppx"], df_dvcs["Ppy"], df_dvcs["Ppz"]])
         cut_common = cut_xBupper & cut_xBlower & cut_Q2 & cut_W & cut_Ee & cut_Ge2 & cut_Esector & cut_Psector & cut_Ppmax
@@ -785,7 +785,7 @@ class root2pickle():
             cut_Ge2 = df_Rec["Ge2"] > 0.6  # Ge cut. Ge>3 for DVCS module.
             cut_Esector = (df_Rec["Esector"]!=df_Rec["Gsector"]) & (df_Rec["Esector"]!=df_Rec["Gsector2"]) 
             cut_Psector = ~( ((df_Rec["Pstat"]//10)%10>0) & (df_Rec["Psector"]==df_Rec["Gsector"]))
-                            & ~( ((df_Rec["Pstat"]//10)%10>0) & df_Rec["Psector"]!=df_Rec["Gsector2"])
+                        & ~( ((df_Rec["Pstat"]//10)%10>0) & df_Rec["Psector"]!=df_Rec["Gsector2"])
             cut_Ppmax = df_Rec.Pp < 0.8  # Pp
             # cut_Vz = np.abs(df_dvcs["Evz"] - df_dvcs["Pvz"]) < 2.5 + 2.5 / mag([df_dvcs["Ppx"], df_dvcs["Ppy"], df_dvcs["Ppz"]])
             cut_common = cut_xBupper & cut_xBlower & cut_Q2 & cut_W & cut_Ee & cut_Ge2 & cut_Esector & cut_Psector & cut_Ppmax
@@ -843,7 +843,7 @@ class root2pickle():
             cut_Ge = df_Rec["Ge"] > 3  # Ge
             cut_Esector = (df_Rec["Esector"]!=df_Rec["Gsector"]) & (df_Rec["Esector"]!=df_Rec["Gsector2"]) 
             cut_Psector = ~( ((df_Rec["Pstat"]//10)%10>0) & (df_Rec["Psector"]==df_Rec["Gsector"]))
-                            & ~( ((df_Rec["Pstat"]//10)%10>0) & df_Rec["Psector"]!=df_Rec["Gsector2"])
+                        & ~( ((df_Rec["Pstat"]//10)%10>0) & df_Rec["Psector"]!=df_Rec["Gsector2"])
             cut_Ppmax = df_Rec.Pp < 0.8  # Pp
             # cut_Vz = np.abs(df_Rec["Evz"] - df_Rec["Pvz"]) < 2.5 + 2.5 / mag([df_Rec["Ppx"], df_Rec["Ppy"], df_Rec["Ppz"]])
             cut_common = cut_xBupper & cut_xBlower & cut_Q2 & cut_W & cut_Ee & cut_Ge & cut_Esector & cut_Psector & cut_Ppmax
