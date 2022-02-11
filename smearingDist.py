8f5bb56c75cef69a68ec242a025791197fdb2c14
Sangbaek <sangbaek@mit.edu>
2022-02-11 15:30:01 -0500

    define photon

--- smearingDist.py
+++ smearingDist.py
@@ -313,6 +313,7 @@ class smearingDist():
 			df_epg.loc[df_epg.Gsector>7, 'Ge'] = df_epg.loc[df_epg.Gsector>7, 'Gp']
 			df_epg.loc[:, 'Gtheta'] = getTheta(gam)
 			df_epg.loc[:, 'Gphi'] = getPhi(gam)
+			gam = [df_epg['Gpx'], df_epg['Gpy'], df_epg['Gpz']]
 			df_epg.loc[:, "Gpx"] = df_epg.loc[:, "Gp"]*np.sin(np.radians(df_epg.loc[:, "Gtheta"]))*np.cos(np.radians(df_epg.loc[:, "Gphi"]))
 			df_epg.loc[:, "Gpy"] = df_epg.loc[:, "Gp"]*np.sin(np.radians(df_epg.loc[:, "Gtheta"]))*np.sin(np.radians(df_epg.loc[:, "Gphi"]))
 			df_epg.loc[:, "Gpz"] = df_epg.loc[:, "Gp"]*np.cos(np.radians(df_epg.loc[:, "Gtheta"]))
