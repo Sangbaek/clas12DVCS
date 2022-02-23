sign mistake

root2pickleEpggExp.py
@@ -384,7 +384,7 @@ class root2pickle():
                 df_gammaRec.loc[cond, "Gp"] = df_gammaRec.loc[cond, "Gp"] + FD_phot_corr_minor_sector
 
             if pol == "outbending":
-                FD_phot_corr_marginal = 0.05*(df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-2)*(df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-5)/(1+np.exp((df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-5)/0.25))/(1+np.exp(-(df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-2)/0.25))
+                FD_phot_corr_marginal = -0.05*(df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-2)*(df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-5)/(1+np.exp((df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-5)/0.25))/(1+np.exp(-(df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"]-2)/0.25))
                 df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"] = df_gammaRec.loc[df_gammaRec.Gsector<7, "Gp"] + FD_phot_corr_marginal
 
             df_gammaRec.loc[:, "Gpx"] = df_gammaRec.loc[:, "Gp"]*np.sin(np.radians(df_gammaRec.loc[:, "Gtheta"]))*np.cos(np.radians(df_gammaRec.loc[:, "Gphi"]))