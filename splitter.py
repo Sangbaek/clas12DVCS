fc0b6bd51928ba2708bb937265dbe673d8b2c3e3
Sangbaek <sangbaek@mit.edu>
2022-04-30 14:31:48 -0400

    split the df in some binning way. initially supports Q2xBt only

---
+++ splitter.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import argparse
+from utils.const import *
+from utils.physics import *
+import subprocess, os
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Get args",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-f","--fname", help="file to be split", default="/Users/sangbaek/Dropbox (MIT)/data/project/merged_9628_files.root")
+    parser.add_argument("-b","--binning", help="Q2, Q2xB, Q2xBt, Q2xBtphi", default="Q2xBt")
+    
+    args = parser.parse_args()
+
+    fname = args.fname
+    # df = pd.read_pickle(args.fname)
+
+    dirname = fname.rsplit('/')[:-1] + "/{}/".format(args.binning)
+    jobn = fname.rsplit('/')[-1][:-4]
+
+    if args.binning == "Q2xBt":
+        for Q2xBtbin in (df.Q2xBtbin).unique():
+            if Q2xBtbin == -1:
+                continue
+            df_sub = df.loc[df.Q2xBtbin == Q2xBtbin, :]
+            ofname = dirname + jobn+"_{}".format(Q2xBtbin)
+            df_sub.to_pickle(ofname)
+
+    # df.loc[:, "BHrad"] = XsecObs
+    # df.loc[:, "BHborn"] = XsecBorn
+    # df.to_pickle(args.ofname)
