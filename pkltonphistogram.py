1f6be87f9dba69cfc311d47ddbac06544fcb4823
Sangbaek <sangbaek@mit.edu>
2022-06-04 15:35:42 -0400

    print files to read

--- pkltonphistogram.py
+++ pkltonphistogram.py
@@ -26,19 +26,6 @@ if __name__ == "__main__":
 
 	jobnum = int(args.jobnum)
 
-	runs_inb_vgg50nA = [3987, 4124, 4139, 4181, 4182, 4397, 4528, 4529, 4535, 4539]
-	runs_inb_vgg55nA = [4186, 4545]
-	runs_inb_vgg45nA = [4188, 4547]
-	runs_inb_vgg0nA = [4192, 4561]
-	runs_inb_bh50nA = [4238, 4542]
-	runs_inb_bh45nA = [4740, 4742, 4745, 4751, 4760]
-
-	runs_outb_vgg50nA = [4240, 4250, 4251, 4252, 4255, 4398, 4532, 4534, 4540, 4541, 4717]
-	runs_outb_vgg40nA = [4263, 4546]
-	runs_outb_vgg0nA = [4262, 4554]
-	runs_outb_vgg40nAT = [4266, 4562]
-	runs_outb_bh50nA = [4249, 4544, 4780, 4808, 4812, 4818]
-
 
 	if jobnum in [*runs_inb_vgg50nA, *runs_inb_vgg55nA, *runs_inb_vgg45nA, *runs_inb_vgg0nA]:
 		pol = "inb"
@@ -61,8 +48,9 @@ if __name__ == "__main__":
 	else:
 		recgen = "Rec"
 
-	df = pd.read_pickle("{}/{}/{}/{}.pkl".format(args.uri, pol, mode, jobnum))
-
+	infile = "{}/{}/{}/{}.pkl".format(args.uri, pol, mode, jobnum)
+	print("reading {}".format(infile))
+	df = pd.read_pickle(infile)
 
 	out = "/volatile/clas12/sangbaek/clas12DVCS/nphistograms/{}{}.npz".format(jobnum, recgen)
 	hist, _ = np.histogramdd(df.loc[: , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins])
