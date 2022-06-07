b136ce2360357f5f49f0cd00a9a4e31dff043554
Sangbaek <sangbaek@mit.edu>
2022-06-07 11:58:57 -0400

    dtype resolved

--- lund2pickleRadGen2.py
+++ lund2pickleRadGen2.py
@@ -61,7 +61,7 @@ class lund2pickle():
 			t1 = eleQuantities[10]
 			phi1 = proQuantities[1]
 			radMode = eleQuantities[5]
-			kinArray.append([float(xB), float(Q2), float(t1), np.degrees(np.pi - float(phi1)), float(xsec), int(radMode), int(helicity)])
+			kinArray.append([float(xB), float(Q2), float(t1), np.degrees(np.pi - float(phi1)), float(xsec), int(float(radMode), int(helicity))])
 
 		df_epgg = pd.DataFrame(kinArray, columns = ["xB", "Q2", "t1", "phi1", "xsec", "radMode", "helicity"])
 
