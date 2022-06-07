fd7fe389756f0e5a5300334201c7dcd8d18f608e
Sangbaek <sangbaek@mit.edu>
2022-06-07 19:29:12 -0400

    select the bin scheme

--- physicsReseults.py
+++ physicsReseults.py
@@ -768,6 +768,8 @@ if args.savexsec:
 
 if args.saveplot:
 
+	k = 0
+
 	phi1avg_VGG = np.load("nphistograms/binscheme{}/phi1avg_VGG.npz".format(k))["hist"]
 	xBavg_VGG   = np.load("nphistograms/binscheme{}/xBavg_VGG.npz".format(k))["hist"]
 	Q2avg_VGG   = np.load("nphistograms/binscheme{}/Q2avg_VGG.npz".format(k))["hist"]
