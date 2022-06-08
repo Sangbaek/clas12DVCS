58be6b25c5cd5cfa497cbc422b80f4806b5348b6
Sangbaek Lee <sangbaek@jlab.org>
2022-06-07 20:51:31 -0400

    working at farm

--- physicsReseults.py
+++ physicsReseults.py
@@ -810,7 +810,7 @@ if args.saveplot:
 	for tbin in range(len(tbins) -1):
 		active = 0
 		ttitle = "{:.3f}".format(tbins[tbin])+r"$<|t|<$"+"{:.3f}".format(tbins[tbin+1])
-		fig, axs = plt.subplots(6-1, 3-1, figsize = (50, 30))
+		fig, axs = plt.subplots(6-1, 3-1, figsize = (15, 30))
 		for xBbin in range(3 - 1):
 			for Q2bin in range(6 - 1):
 				#skip inactive bins
@@ -838,7 +838,7 @@ if args.saveplot:
 				if active == 0:
 					handles, labels = axs[6-Q2bin-2, xBbin].get_legend_handles_labels()
 					active = 1
-		lgd = plt.figlegend(handles, labels, loc='right', fontsize= 20, title = ttitle, bbox_to_anchor = (1.0, 0.6))
+		lgd = plt.figlegend(handles, labels, loc='upper left', fontsize= 20, title = ttitle, bbox_to_anchor = (1.0, 0.6))
 		fig.subplots_adjust(wspace = 0.7, hspace = 0.7)
 		plt.savefig("plots/binscheme{}/bkgscheme{}tbin{}.pdf".format(k, i, tbin), bbox_extra_artists=[lgd], bbox_inches = 'tight')
 		plt.clf()
