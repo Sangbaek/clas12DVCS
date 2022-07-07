446504e27a1261c916ad27200f7bd487cea0091a
Sangbaek <sangbaek@mit.edu>
2022-07-07 00:07:24 -0400

    save syst2

--- physicsResults.py
+++ physicsResults.py
@@ -1826,9 +1826,9 @@ if args.savesyst2:
 
 	axs.errorbar(phi1avg_BH[xBbin, Q2bin, tbin, phibin], (xsec_BH/Normalization)[xBbin, Q2bin, tbin, phibin], xerr = [phi1avg_BH[xBbin, Q2bin, tbin, phibin]-phibins[:-1][phibin], phibins[1:][phibin]-phi1avg_BH[xBbin, Q2bin, tbin, phibin]], yerr = (uncStat_BH*xsec_BH)[xBbin, Q2bin, tbin, phibin], linestyle ='', color = 'k', label = 'Experimental data')
 	for i in range(-500, 500):
-		axs.plot(phi1avg_BH[xBbin, Q2bin, tbin, phibin], (1+i*SystUnc/500)*(xsec_BH/Normalization)[xBbin, Q2bin, tbin, phibin], color = 'g', alpha = 1/500)
+		axs.plot(phi1avg_BH[xBbin, Q2bin, tbin, phibin], (1+i*SystUnc/500)*(xsec_BH/Normalization)[xBbin, Q2bin, tbin, phibin], color = 'g', alpha = 1/100)
 
-	axs.plot(phi1avg_BH[xBbin, Q2bin, tbin, phibin], 1/(P1b*P2b)*FourierSeries(res_lsq.x, phi1avg_BH[xBbin, Q2bin, tbin, phibin]), label = 'Fitting results', color = 'k', linestyle = '--')
+	axs.plot(phi1avg_BH[xBbin, Q2bin, tbin, phibin], 1/(P1b*P2b*Normalization)*FourierSeries(res_lsq.x, phi1avg_BH[xBbin, Q2bin, tbin, phibin]), label = 'Fitting results', color = 'k', linestyle = '--')
 	axs.plot(phi1avg_BH[xBbin, Q2bin, tbin, phibin], xsecTh_KM[xBbin, Q2bin, tbin, phibin], color = 'cyan', label = 'Theory (KM15)')
 	axs.plot(phi1avg_BH[xBbin, Q2bin, tbin, phibin], xsecTh_BH[xBbin, Q2bin, tbin, phibin], color = 'r', label = 'Theory (BH)')
 
