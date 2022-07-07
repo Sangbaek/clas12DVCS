d4af13e8f15b5a733ab9d95f4fe9f64c92a64d28
Sangbaek <sangbaek@mit.edu>
2022-07-06 22:56:54 -0400

    try normalization

--- physicsResults.py
+++ physicsResults.py
@@ -1627,6 +1627,13 @@ if args.savesyst:
 			axs.plot(phi1s, FourierSeries(res_lsq.x, phi1s), label = 'Fitting results {}'.format(optionaltag[1:]), color = colorscheme[scheme], linestyle = '--')
 		scheme = scheme+1
 
+	P1_zero = P1(xBavg_BH[xBbin, Q2bin, tbin, 0], Q2avg_BH[xBbin, Q2bin, tbin, 0], t1avg_BH[xBbin, Q2bin, tbin, 0], 0)
+	P2_zero = P2(xBavg_BH[xBbin, Q2bin, tbin, 0], Q2avg_BH[xBbin, Q2bin, tbin, 0], t1avg_BH[xBbin, Q2bin, tbin, 0], 0)
+	BHDVCS_zero = getBHDVCS(xBavg_BH[xBbin, Q2bin, tbin, 0], Q2avg_BH[xBbin, Q2bin, tbin, 0], t1avg_BH[xBbin, Q2bin, tbin, 0], 0, mode = 1)
+	Normalization = FourierSeries(res_lsq.x, 0)/(P1_zero*P2_zero*BHDVCS_zero)
+
+	print(P1_zero, P2_zero, BHDVCS_zero, Normalization, FourierSeries(res_lsq.x, 0), np.sum(res_lsq.x))
+
 	handles, labels = axs.get_legend_handles_labels()
 	lgd = plt.figlegend(handles, labels, loc='upper left', fontsize= 20, bbox_to_anchor = (1.0, 0.8))
 	axs.set_xlim([0, 360])
