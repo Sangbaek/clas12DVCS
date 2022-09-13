8f1c6e4e7255ebff8c1b6ae035ce898fb4c40f9f
Sangbaek <sangbaek@mit.edu>
2022-09-13 16:19:58 -0500

    save logistics of spring 2019

--- root2csvLogistics.py
+++ root2csvLogistics.py
@@ -19,18 +19,35 @@ df_logistics3 = pd.DataFrame()
 file3 = uproot.open("/volatile/clas12/sangbaek/nov2021/convROOT_eb/exp/outb/dvcs.root")
 df_logistics4 = pd.DataFrame()
 file4 = uproot.open("/volatile/clas12/sangbaek/nov2021/convROOT_eb/exp/outb/pi0.root")
+df_logistics5 = pd.DataFrame()
+file5 = uproot.open("/volatile/clas12/sangbaek/pass1_test/convROOT_eb/exp/inb/dvcs.root")
+df_logistics6 = pd.DataFrame()
+file6 = uproot.open("/volatile/clas12/sangbaek/pass1_test/convROOT_eb/exp/inb/pi0.root")
+df_logistics7 = pd.DataFrame()
+file7 = uproot.open("/volatile/clas12/sangbaek/pass2_test/convROOT_eb/exp/inb/dvcs.root")
+df_logistics8 = pd.DataFrame()
+file8 = uproot.open("/volatile/clas12/sangbaek/pass2_test/convROOT_eb/exp/inb/pi0.root")
+
 tree1 = file1["T"]
 tree2 = file2["T"]
 tree3 = file3["T"]
 tree4 = file4["T"]
+tree5 = file5["T"]
+tree6 = file6["T"]
+tree7 = file7["T"]
+tree8 = file8["T"]
 
 for key in ["RunNum", "EventNum", "beamQ"]:
     df_logistics1[key] = tree1[key].array(library="pd")
     df_logistics2[key] = tree2[key].array(library="pd")
     df_logistics3[key] = tree3[key].array(library="pd")
     df_logistics4[key] = tree4[key].array(library="pd")
+    df_logistics5[key] = tree5[key].array(library="pd")
+    df_logistics6[key] = tree6[key].array(library="pd")
+    df_logistics7[key] = tree7[key].array(library="pd")
+    df_logistics8[key] = tree8[key].array(library="pd")
 
-df = pd.concat([df_logistics1, df_logistics2, df_logistics3, df_logistics4]).drop_duplicates().reset_index(drop=True)
+df = pd.concat([df_logistics1, df_logistics2, df_logistics3, df_logistics4, df_logistics5, df_logistics6, df_logistics7, df_logistics8]).drop_duplicates().reset_index(drop=True)
 df = df.sort_values(by=['RunNum', 'EventNum'], ascending = [True, True])
 
 for run in df.RunNum.unique():
