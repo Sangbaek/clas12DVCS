2ae00be2c79b97cd6d08667332cd0e102742164c
Sangbeak Lee <sangbaek.lee@anl.gov>
2022-09-29 20:18:20 -0500

    Add Outb raw yields

--- plot_acceptances.py
+++ plot_acceptances.py
@@ -81,6 +81,19 @@ histBHDVCSInbCR, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == -1) & (epg
 histExpInb = histExpInbFD + histExpInbCD + histExpInbCDFT + histExpInbCR
 histBHDVCSInb = histBHDVCSInbFD + histBHDVCSInbCD + histBHDVCSInbCDFT + histBHDVCSInbCR
 
+histExpOutbFD, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 1) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins])
+histExpOutbCD, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 2) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins])
+histExpOutbCDFT, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 3) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins])
+histExpOutbCR, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 4) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins])
+histBHDVCSOutbFD, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 1) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins], weights = 1 - epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 1), "cont{}".format(i)])
+histBHDVCSOutbCD, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 2) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins], weights = 1 - epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 2), "cont{}".format(i)])
+histBHDVCSOutbCDFT, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 3) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins], weights = 1 - epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 3), "cont{}".format(i)])
+histBHDVCSOutbCR, bins = np.histogramdd(epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 4) , ["xB", "Q2", "t1", "phi1"]].to_numpy(), bins = [xBbins, Q2bins, tbins, phibins], weights = 1 - epgExp.loc[(epgExp.polarity == 1) & (epgExp.config == 4), "cont{}".format(i)])
+
+histExpOutb = histExpOutbFD + histExpOutbCD + histExpOutbCDFT
+histBHDVCSOutb = histBHDVCSOutbFD + histBHDVCSOutbCD + histBHDVCSOutbCDFT
+
+
 print("reading bhs - inbending ")
 
 # Count Rec BH
@@ -121,6 +134,53 @@ for jobNum in [*runs_inb_vgg45nA, *runs_inb_vgg50nA, *runs_inb_vgg55nA]:
 	histVGGGenInbCDFT45nA = histVGGGenInbCDFT45nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen3.npz".format(k, jobNum))["hist"]
 	histVGGGenInbCR45nA = histVGGGenInbCR45nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen4.npz".format(k, jobNum))["hist"]
 
+print("reading bhs  - outbending")
+# bh rec
+histBHOutb50nA, histBHOutbFD50nA, histBHOutbCD50nA, histBHOutbCDFT50nA, histBHOutbCR50nA = 0, 0, 0, 0, 0
+histBHOutb50nA_plus, histBHOutbFD50nA_plus, histBHOutbCD50nA_plus, histBHOutbCDFT50nA_plus, histBHOutbCR50nA_plus = 0, 0, 0, 0, 0
+histBHOutb50nA_minus, histBHOutbFD50nA_minus, histBHOutbCD50nA_minus, histBHOutbCDFT50nA_minus, histBHOutbCR50nA_minus = 0, 0, 0, 0, 0
+for jobNum in [*runs_outb_bh50nA, *runs_outb_bh40nA, *runs_outb_bh40nAT]:
+	histBHOutb50nA = histBHOutb50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec.npz".format(optionaltag, k, jobNum))["hist"]
+	histBHOutbFD50nA = histBHOutbFD50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec1.npz".format(optionaltag, k, jobNum))["hist"]
+	histBHOutbCD50nA = histBHOutbCD50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec2.npz".format(optionaltag, k, jobNum))["hist"]
+	histBHOutbCDFT50nA = histBHOutbCDFT50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec3.npz".format(optionaltag, k, jobNum))["hist"]
+	histBHOutbCR50nA = histBHOutbCR50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec4.npz".format(optionaltag, k, jobNum))["hist"]
+
+# bh gen
+histBHGenOutb50nA, histBHGenOutbFD50nA, histBHGenOutbCD50nA, histBHGenOutbCDFT50nA, histBHGenOutbCR50nA = 0, 0, 0, 0, 0
+histBHGenOutb50nA_plus, histBHGenOutbFD50nA_plus, histBHGenOutbCD50nA_plus, histBHGenOutbCDFT50nA_plus, histBHGenOutbCR50nA_plus = 0, 0, 0, 0, 0
+histBHGenOutb50nA_minus, histBHGenOutbFD50nA_minus, histBHGenOutbCD50nA_minus, histBHGenOutbCDFT50nA_minus, histBHGenOutbCR50nA_minus = 0, 0, 0, 0, 0
+for jobNum in [*runs_outb_bh50nA, *runs_outb_bh40nA, *runs_outb_bh40nAT]:
+	histBHGenOutb50nA = histBHGenOutb50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen.npz".format(k, jobNum))["hist"]
+	histBHGenOutbFD50nA = histBHGenOutbFD50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen1.npz".format(k, jobNum))["hist"]
+	histBHGenOutbCD50nA = histBHGenOutbCD50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen2.npz".format(k, jobNum))["hist"]
+	histBHGenOutbCDFT50nA = histBHGenOutbCDFT50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen3.npz".format(k, jobNum))["hist"]
+	histBHGenOutbCR50nA = histBHGenOutbCR50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen4.npz".format(k, jobNum))["hist"]
+
+print("reading vggs  - outbending")
+# vgg rec
+histVGGOutb50nA, histVGGOutbFD50nA, histVGGOutbCD50nA, histVGGOutbCDFT50nA, histVGGOutbCR50nA = 0, 0, 0, 0, 0
+histVGGOutb50nA_plus, histVGGOutbFD50nA_plus, histVGGOutbCD50nA_plus, histVGGOutbCDFT50nA_plus, histVGGOutbCR50nA_plus = 0, 0, 0, 0, 0
+histVGGOutb50nA_minus, histVGGOutbFD50nA_minus, histVGGOutbCD50nA_minus, histVGGOutbCDFT50nA_minus, histVGGOutbCR50nA_minus = 0, 0, 0, 0, 0
+for jobNum in [*runs_outb_vgg50nA, *runs_outb_vgg40nA, *runs_outb_vgg40nAT]:
+	histVGGOutb50nA = histVGGOutb50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec.npz".format(optionaltag, k, jobNum))["hist"]
+	histVGGOutbFD50nA = histVGGOutbFD50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec1.npz".format(optionaltag, k, jobNum))["hist"]
+	histVGGOutbCD50nA = histVGGOutbCD50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec2.npz".format(optionaltag, k, jobNum))["hist"]
+	histVGGOutbCDFT50nA = histVGGOutbCDFT50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec3.npz".format(optionaltag, k, jobNum))["hist"]
+	histVGGOutbCR50nA = histVGGOutbCR50nA + np.load(basedir + "/nphistograms{}/binscheme{}/{}Rec4.npz".format(optionaltag, k, jobNum))["hist"]
+
+#vgg gen
+histVGGGenOutb50nA, histVGGGenOutbFD50nA, histVGGGenOutbCD50nA, histVGGGenOutbCDFT50nA, histVGGGenOutbCR50nA = 0, 0, 0, 0, 0
+histVGGGenOutb50nA_plus, histVGGGenOutbFD50nA_plus, histVGGGenOutbCD50nA_plus, histVGGGenOutbCDFT50nA_plus, histVGGGenOutbCR50nA_plus = 0, 0, 0, 0, 0
+histVGGGenOutb50nA_minus, histVGGGenOutbFD50nA_minus, histVGGGenOutbCD50nA_minus, histVGGGenOutbCDFT50nA_minus, histVGGGenOutbCR50nA_minus = 0, 0, 0, 0, 0
+for jobNum in [*runs_outb_vgg50nA, *runs_outb_vgg40nA, *runs_outb_vgg40nAT]:
+	histVGGGenOutb50nA = histVGGGenOutb50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen.npz".format(k, jobNum))["hist"]
+	histVGGGenOutbFD50nA = histVGGGenOutbFD50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen1.npz".format(k, jobNum))["hist"]
+	histVGGGenOutbCD50nA = histVGGGenOutbCD50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen2.npz".format(k, jobNum))["hist"]
+	histVGGGenOutbCDFT50nA = histVGGGenOutbCDFT50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen3.npz".format(k, jobNum))["hist"]
+	histVGGGenOutbCR50nA = histVGGGenOutbCR50nA + np.load(basedir + "/nphistograms/binscheme{}/{}Gen4.npz".format(k, jobNum))["hist"]
+
+
 #acc correction with stat error - inbending
 #with VGG
 accCorrectedInbFD_VGG = divideHist(histBHDVCSInbFD*histVGGGenInbFD45nA , histVGGInbFD45nA)
@@ -153,10 +213,46 @@ accCorrectedInb_BH2[:, :, 0, :] = divideHist((histBHDVCSInbCD+histBHDVCSInbCDFT+
 accCorrectedInb_BH = divideHist(accCorrectedInb_BH*histBHGenInb45nA, histBHGenInbFD45nA + histBHGenInbCD45nA + histBHGenInbCDFT45nA + histBHGenInbCR45nA)
 accCorrectedInb_BH[:, :, 0, :] = divideHist(accCorrectedInb_BH[:, :, 0, :]*histBHGenInb45nA[:, :, 0, :], histBHGenInbCD45nA[:, :, 0, :] + histBHGenInbCDFT45nA[:, :, 0, :] + histBHGenInbCR45nA[:, :, 0, :])
 
+# acceptance correction with stat error - outbending
+# with VGG
+accCorrectedOutbFD_VGG = divideHist(histBHDVCSOutbFD*histVGGGenOutbFD50nA , histVGGOutbFD50nA)
+accCorrectedOutbCD_VGG = divideHist(histBHDVCSOutbCD*histVGGGenOutbCD50nA , histVGGOutbCD50nA)
+accCorrectedOutbCDFT_VGG = divideHist(histBHDVCSOutbCDFT*histVGGGenOutbCDFT50nA , histVGGOutbCDFT50nA)
+accCorrectedOutbCR_VGG = divideHist(histBHDVCSOutbCR*histVGGGenOutbCR50nA , histVGGOutbCR50nA)
+accCorrectedOutb_VGG = accCorrectedOutbFD_VGG + accCorrectedOutbCD_VGG + accCorrectedOutbCDFT_VGG + accCorrectedOutbCR_VGG
+accCorrectedOutb_VGG2 = divideHist((histBHDVCSOutbFD+histBHDVCSOutbCD+histBHDVCSOutbCDFT+histBHDVCSOutbCR)*histVGGGenOutb50nA , histVGGOutbFD50nA+histVGGOutbCD50nA+histVGGOutbCDFT50nA+histVGGOutbCR50nA)
+#FD inbending threshold [:, :, [0, 1], :]
+accCorrectedOutb_VGG[:, :, [0, 1], :] = accCorrectedOutbCD_VGG[:, :, [0, 1], :] + accCorrectedOutbCDFT_VGG[:, :, [0, 1], :] + accCorrectedOutbCR_VGG[:, :, [0, 1], :]
+accCorrectedOutb_VGG2[:, :, [0, 1], :] = divideHist((histBHDVCSOutbCD+histBHDVCSOutbCDFT+histBHDVCSOutbCR)*histVGGGenOutb50nA , histVGGOutbCD50nA+histVGGOutbCDFT50nA+histVGGOutbCR50nA)[:, :, [0, 1], :]
+
+#Correct by the ratio of covered/uncovered
+accCorrectedOutb_VGG = divideHist(accCorrectedOutb_VGG*histVGGGenOutb50nA, histVGGGenOutbFD50nA + histVGGGenOutbCD50nA + histVGGGenOutbCDFT50nA + histVGGGenOutbCR50nA)
+accCorrectedOutb_VGG[:, :, [0, 1], :] = divideHist(accCorrectedOutb_VGG[:, :, [0, 1], :]*histVGGGenOutb50nA[:, :, [0, 1], :], histVGGGenOutbCD50nA[:, :, [0, 1], :] + histVGGGenOutbCDFT50nA[:, :, [0, 1], :] + histVGGGenOutbCR50nA[:, :, [0, 1], :])
+
+# acc correction with BH, outbending
+accCorrectedOutbFD_BH = divideHist(histBHDVCSOutbFD*histBHGenOutbFD50nA , histBHOutbFD50nA)
+accCorrectedOutbCD_BH = divideHist(histBHDVCSOutbCD*histBHGenOutbCD50nA , histBHOutbCD50nA)
+accCorrectedOutbCDFT_BH = divideHist(histBHDVCSOutbCDFT*histBHGenOutbCDFT50nA , histBHOutbCDFT50nA)
+accCorrectedOutbCR_BH = divideHist(histBHDVCSOutbCR*histBHGenOutbCR50nA , histBHOutbCR50nA)
+accCorrectedOutb_BH = accCorrectedOutbFD_BH + accCorrectedOutbCD_BH + accCorrectedOutbCDFT_BH + accCorrectedOutbCR_BH
+accCorrectedOutb_BH2 = divideHist((histBHDVCSOutbFD+histBHDVCSOutbCD+histBHDVCSOutbCDFT+histBHDVCSOutbCR)*histBHGenOutb50nA , histBHOutbFD50nA+histBHOutbCD50nA+histBHOutbCDFT50nA+histBHOutbCR50nA)
+#FD inbending threshold [:, :, [0, 1], :]
+accCorrectedOutb_BH[:, :, [0, 1], :] = accCorrectedOutbCD_BH[:, :, [0, 1], :] + accCorrectedOutbCDFT_BH[:, :, [0, 1], :] + accCorrectedOutbCR_BH[:, :, [0, 1], :]
+accCorrectedOutb_BH2[:, :, [0, 1], :] = divideHist((histBHDVCSOutbCD+histBHDVCSOutbCDFT+histBHDVCSOutbCR)*histBHGenOutb50nA , histBHOutbCD50nA+histBHOutbCDFT50nA+histBHOutbCR50nA)[:, :, [0, 1], :]
+
+#Correct by the ratio of covered/uncovered
+accCorrectedOutb_BH = divideHist(accCorrectedOutb_BH*histBHGenOutb50nA, histBHGenOutbFD50nA + histBHGenOutbCD50nA + histBHGenOutbCDFT50nA + histBHGenOutbCR50nA)
+accCorrectedOutb_BH[:, :, [0, 1], :] = divideHist(accCorrectedOutb_BH[:, :, [0, 1], :]*histBHGenOutb50nA[:, :, [0, 1], :], histBHGenOutbCD50nA[:, :, [0, 1], :] + histBHGenOutbCDFT50nA[:, :, [0, 1], :] + histBHGenOutbCR50nA[:, :, [0, 1], :])
+
 ActiveAll       = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveAll.npz".format('', k, i))["hist"]
 ActiveAny       = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveAny.npz".format('', k, i))["hist"]
 ActiveInb       = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveInb.npz".format('', k, i))["hist"]
 ActiveOutb      = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveOutb.npz".format('', k, i))["hist"]
+
+ActiveAll_int       = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveAll_int.npz".format(optionaltag, k, i))["hist"]
+ActiveAny_int       = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveAny_int.npz".format(optionaltag, k, i))["hist"]
+ActiveInb_int       = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveInb_int.npz".format(optionaltag, k, i))["hist"]
+ActiveOutb_int      = np.load(basedir + "/nphistograms{}/binscheme{}/bkgscheme{}ActiveOutb_int.npz".format(optionaltag, k, i))["hist"]
 
 
 def badBinCondxBQ2t(xBbin, Q2bin, tbin, k = 0):
