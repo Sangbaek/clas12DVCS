57fc2537dba586b3be607bfb4e05b4285760f3ee
Sangbeak Lee <sangbaek.lee@anl.gov>
2022-10-03 09:58:03 -0500

    Changes tab to spaces

--- plot_normalization.py
+++ plot_normalization.py
@@ -20,20 +20,20 @@ cmap.set_bad('w',0)
 import matplotlib
 # initial settings
 pgf_with_latex = {
-		"pgf.texsystem": "pdflatex",
-		"text.usetex": True,			# use LaTeX to write all text
-		"font.family": "sans-serif",		
-		"font.sans-serif": "Helvetica",
-		"font.size": 25,				# default font size
-		"axes.titlepad": 20,			# x and y label size
-		"axes.labelsize": 24,			# x and y label size
-		"axes.titlesize": 24,		  # subfigure title size, i.e. title size when one figure
-		"legend.fontsize": 22,			# legend size
-		"xtick.labelsize": 23,			# x axis tick label size
-		"ytick.labelsize": 23,			# y axis tick label 
-		"figure.titlesize": 25,         # Figure title size, useful when you have multiple plots in one canvas.
-		"pgf.preamble": r"\usepackage{xcolor}",     # xcolor for colours
-		"figure.autolayout": False
+        "pgf.texsystem": "pdflatex",
+        "text.usetex": True,			# use LaTeX to write all text
+        "font.family": "sans-serif",		
+        "font.sans-serif": "Helvetica",
+        "font.size": 25,				# default font size
+        "axes.titlepad": 20,			# x and y label size
+        "axes.labelsize": 24,			# x and y label size
+        "axes.titlesize": 24,			# subfigure title size, i.e. title size when one figure
+        "legend.fontsize": 22,			# legend size
+        "xtick.labelsize": 23,			# x axis tick label size
+        "ytick.labelsize": 23,			# y axis tick label 
+        "figure.titlesize": 25,			# Figure title size, useful when you have multiple plots in one canvas.
+        "pgf.preamble": r"\usepackage{xcolor}",     # xcolor for colours
+        "figure.autolayout": False
 }
 matplotlib.rcParams.update(pgf_with_latex)
 
@@ -79,9 +79,9 @@ pi0SimInbFD = pi0SimInb.loc[(pi0SimInb.config == 1)]
 
 contInbCDFT = len(pi0ExpInbCDFT)*len(bkgSimInbCDFT)/len(pi0SimInbCDFT)/len(epgExpInbCDFT)
 contInbCD = (len(pi0ExpInbCD.loc[(pi0ExpInbCD.phi1<30)|(pi0ExpInbCD.phi1>330)]) *
-			len(bkgSimInbCD.loc[(bkgSimInbCD.phi1<30)|(bkgSimInbCD.phi1>330)]) /
-			len(pi0SimInbCD.loc[(pi0SimInbCD.phi1<30)|(pi0SimInbCD.phi1>330)]) /
-			len(epgExpInbCD.loc[(epgExpInbCD.phi1<30)|(epgExpInbCD.phi1>330)]))
+            len(bkgSimInbCD.loc[(bkgSimInbCD.phi1<30)|(bkgSimInbCD.phi1>330)]) /
+            len(pi0SimInbCD.loc[(pi0SimInbCD.phi1<30)|(pi0SimInbCD.phi1>330)]) /
+            len(epgExpInbCD.loc[(epgExpInbCD.phi1<30)|(epgExpInbCD.phi1>330)]))
 contInbFD = len(pi0ExpInbFD)*len(bkgSimInbFD)/len(pi0SimInbFD)/len(epgExpInbFD)
 
 varstoplot = ["xB", "Q2", "t1", "Ep", "Etheta", "Ephi", "Pp", "Ptheta", "Pphi", "Gp", "Gtheta", "Gphi"]
@@ -91,50 +91,48 @@ unit = ["", GeVc2, GeV2, GeVc, degree, degree, GeVc, degree, degree, GeVc, degre
 
 fig, axs = plt.subplots(4, 3, figsize = (18, 30))
 for yind in range(0, 4):
-	for xind in range(0, 3):
-		ind = 4*yind + xind
-		simDist_dvcs, bins = np.histogram(dvcsSimInbCDFT.loc[:, varstoplot[ind]], binstoplot[ind], density = True)
-		simDist_dvpi0, _ = np.histogram(bkgSimInbCDFT.loc[:, varstoplot[ind]], bins, density = True)
-		simDist = (1-contInbCDFT)*simDist_dvcs + contInbCDFT*simDist_dvpi0
-
-		simDist_bh, _ = np.histogram(bhSimInbCDFT.loc[:, varstoplot[ind]], bins, density = True)
-		simDist2 = (1-contInbCDFT)*simDist_bh + contInbCDFT*simDist_dvpi0
-
-		bincenters = np.array([0.5 * (bins[i] + bins[i + 1]) for i in range(len(bins) - 1)])
-		axs[yind, xind].hist(epgExpInbCDFT.loc[:, varstoplot[ind]], bins = bins, histtype = 'step', edgecolor='b', density=True, linewidth=1, label = "Experimental Data")
-		axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='r', linewidth=1, label = 'Simulation based on VGG')
-		axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='g', linewidth=1, label = 'Simulation based on pure BH')
-		axs[yind, xind].set_title(title[ind])
-		if (unit[ind]):
-			axs[yind, xind].set_xlabel(title[ind]+" [" + unit[ind] +"]")
-		else:
-			axs[yind, xind].set_xlabel(title[ind])
-		axs[yind, xind].set_xlim([bins[0], bins[-1]])
+    for xind in range(0, 3):
+        ind = 4*yind + xind
+        simDist_dvcs, bins = np.histogram(dvcsSimInbCDFT.loc[:, varstoplot[ind]], binstoplot[ind], density = True)
+        simDist_dvpi0, _ = np.histogram(bkgSimInbCDFT.loc[:, varstoplot[ind]], bins, density = True)
+        simDist = (1-contInbCDFT)*simDist_dvcs + contInbCDFT*simDist_dvpi0
+        simDist_bh, _ = np.histogram(bhSimInbCDFT.loc[:, varstoplot[ind]], bins, density = True)
+        simDist2 = (1-contInbCDFT)*simDist_bh + contInbCDFT*simDist_dvpi0
+        bincenters = np.array([0.5 * (bins[i] + bins[i + 1]) for i in range(len(bins) - 1)])
+        axs[yind, xind].hist(epgExpInbCDFT.loc[:, varstoplot[ind]], bins = bins, histtype = 'step', edgecolor='b', density=True, linewidth=1, label = "Experimental Data")
+        axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='r', linewidth=1, label = 'Simulation based on VGG')
+        axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='g', linewidth=1, label = 'Simulation based on pure BH')
+        axs[yind, xind].set_title(title[ind])
+        if (unit[ind]):
+            axs[yind, xind].set_xlabel(title[ind]+" [" + unit[ind] +"]")
+        else:
+            axs[yind, xind].set_xlabel(title[ind])
+        axs[yind, xind].set_xlim([bins[0], bins[-1]])
 
 plt.savefig("plots/normalization/CDFT_particle_kine.pdf")
 plt.clf()
 
 fig, axs = plt.subplots(3, 1, figsize = (12, 6))
 for yind in range(0, 4):
-	for xind in range(0, 3):
-		ind = 4*yind + xind
-		simDist_dvcs, bins = np.histogram(dvcsSimInbCD.loc[(dvcsSimInbCD.phi1<30)|(dvcsSimInbCD.phi1>30), varstoplot[ind]], binstoplot[ind], density = True)
-		simDist_dvpi0, _ = np.histogram(bkgSimInbCD.loc[(bkgSimInbCD.phi1<30)|(bkgSimInbCD.phi1>30), varstoplot[ind]], bins, density = True)
-		simDist = (1-contInbCD)*simDist_dvcs + contInbCD*simDist_dvpi0
+    for xind in range(0, 3):
+        ind = 4*yind + xind
+        simDist_dvcs, bins = np.histogram(dvcsSimInbCD.loc[(dvcsSimInbCD.phi1<30)|(dvcsSimInbCD.phi1>30), varstoplot[ind]], binstoplot[ind], density = True)
+        simDist_dvpi0, _ = np.histogram(bkgSimInbCD.loc[(bkgSimInbCD.phi1<30)|(bkgSimInbCD.phi1>30), varstoplot[ind]], bins, density = True)
+        simDist = (1-contInbCD)*simDist_dvcs + contInbCD*simDist_dvpi0
 
-		simDist_bh, _ = np.histogram(bhSimInbCD.loc[(bhSimInbCD.phi1<30)|(bhSimInbCD.phi1>30), varstoplot[ind]], bins, density = True)
-		simDist2 = (1-contInbCD)*simDist_bh + contInbCD*simDist_dvpi0
+        simDist_bh, _ = np.histogram(bhSimInbCD.loc[(bhSimInbCD.phi1<30)|(bhSimInbCD.phi1>30), varstoplot[ind]], bins, density = True)
+        simDist2 = (1-contInbCD)*simDist_bh + contInbCD*simDist_dvpi0
 
-		bincenters = np.array([0.5 * (bins[i] + bins[i + 1]) for i in range(len(bins) - 1)])
-		axs[yind, xind].hist(epgExpInbCD.loc[(epgExpInbCD.phi1<30)|(epgExpInbCD.phi1>30),varstoplot[ind]], bins = bins, histtype = 'step', edgecolor='b', density=True, linewidth=1, label = "Experimental Data")
-		axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='r', linewidth=1, label = 'Simulation based on VGG')
-		axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='g', linewidth=1, label = 'Simulation based on pure BH')
-		axs[yind, xind].set_title(title[ind])
-		if (unit[ind]):
-			axs[yind, xind].set_xlabel(title[ind]+" [" + unit[ind] +"]")
-		else:
-			axs[yind, xind].set_xlabel(title[ind])
-		axs[yind, xind].set_xlim([bins[0], bins[-1]])
+        bincenters = np.array([0.5 * (bins[i] + bins[i + 1]) for i in range(len(bins) - 1)])
+        axs[yind, xind].hist(epgExpInbCD.loc[(epgExpInbCD.phi1<30)|(epgExpInbCD.phi1>30),varstoplot[ind]], bins = bins, histtype = 'step', edgecolor='b', density=True, linewidth=1, label = "Experimental Data")
+        axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='r', linewidth=1, label = 'Simulation based on VGG')
+        axs[yind, xind].hist(bins[:-1], bins, weights = simDist, histtype = 'step', color='g', linewidth=1, label = 'Simulation based on pure BH')
+        axs[yind, xind].set_title(title[ind])
+        if (unit[ind]):
+            axs[yind, xind].set_xlabel(title[ind]+" [" + unit[ind] +"]")
+        else:
+            axs[yind, xind].set_xlabel(title[ind])
+        axs[yind, xind].set_xlim([bins[0], bins[-1]])
 
 plt.savefig("plots/normalization/CD_particle_kine.pdf")
 plt.clf()
