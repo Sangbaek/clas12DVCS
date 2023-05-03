820fe791d96e254f183b21b1b2aa1503f6781a89
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-05-03 16:36:18 -0500

    Just read from root

--- root2pickleDVCSGen.py
+++ root2pickleDVCSGen.py
@@ -18,7 +18,7 @@ class root2pickle():
         self.fname = fname
 
         self.readEPGG(entry_stop)
-        self.saveDVCSvars()
+        # self.saveDVCSvars()
         # self.saveHistogram()
         self.saveRaw()
 
@@ -40,9 +40,9 @@ class root2pickle():
         df_electronGen = pd.DataFrame()
         df_protonGen = pd.DataFrame()
         df_gammaGen = pd.DataFrame()
-        eleKeysGen = ["GenEpx", "GenEpy", "GenEpz"]
-        proKeysGen = ["GenPpx", "GenPpy", "GenPpz"]
-        gamKeysGen = ["GenGpx", "GenGpy", "GenGpz"]
+        eleKeysGen = ["GenEpx", "GenEpy", "GenEpz", "GenEp", "GenEtheta", "GenEphi", "GenQ2", "GenxB", "Gent", "Genphi"]
+        proKeysGen = ["GenPpx", "GenPpy", "GenPpz", "GenPp", "GenPtheta", "GenPphi"]
+        gamKeysGen = ["GenGpx", "GenGpy", "GenGpz", "GenGp", "GenGtheta", "GenGphi"]
         # read keys
         for key in eleKeysGen:
             df_electronGen[key] = self.tree[key].array(library="pd", entry_stop=entry_stop)
@@ -51,10 +51,10 @@ class root2pickle():
         for key in gamKeysGen:
             df_gammaGen[key] = self.tree[key].array(library="pd", entry_stop=entry_stop)
 
-        #convert data type to standard double
-        df_electronGen = df_electronGen.astype({"GenEpx": float, "GenEpy": float, "GenEpz": float})
-        df_protonGen = df_protonGen.astype({"GenPpx": float, "GenPpy": float, "GenPpz": float})
-        df_gammaGen = df_gammaGen.astype({"GenGpx": float, "GenGpy": float, "GenGpz": float})
+        # #convert data type to standard double
+        # df_electronGen = df_electronGen.astype({"GenEpx": float, "GenEpy": float, "GenEpz": float})
+        # df_protonGen = df_protonGen.astype({"GenPpx": float, "GenPpy": float, "GenPpz": float})
+        # df_gammaGen = df_gammaGen.astype({"GenGpx": float, "GenGpy": float, "GenGpz": float})
 
         #set up a dummy index for merging
         df_electronGen.loc[:,'event'] = df_electronGen.index
@@ -62,101 +62,102 @@ class root2pickle():
         df_gammaGen.loc[:,'event'] = df_gammaGen.index.get_level_values('entry')
 
         #sort columns for readability
-        df_electronGen = df_electronGen.loc[:, ["event", "GenEpx", "GenEpy", "GenEpz"]]
+        column_names = ["event"] + list(df_electronGen[:-1])
+        df_electronGen = df_electronGen.loc[:, column_names]
 
-        #spherical coordinates
-        eleGen = [df_electronGen["GenEpx"], df_electronGen["GenEpy"], df_electronGen["GenEpz"]]
-        df_electronGen.loc[:, 'GenEp'] = mag(eleGen)
-        df_electronGen.loc[:, 'GenEtheta'] = getTheta(eleGen)
-        df_electronGen.loc[:, 'GenEphi'] = getPhi(eleGen)
+        # #spherical coordinates
+        # eleGen = [df_electronGen["GenEpx"], df_electronGen["GenEpy"], df_electronGen["GenEpz"]]
+        # df_electronGen.loc[:, 'GenEp'] = mag(eleGen)
+        # df_electronGen.loc[:, 'GenEtheta'] = getTheta(eleGen)
+        # df_electronGen.loc[:, 'GenEphi'] = getPhi(eleGen)
 
-        proGen = [df_protonGen["GenPpx"], df_protonGen["GenPpy"], df_protonGen["GenPpz"]]
-        df_protonGen.loc[:, 'GenPp'] = mag(proGen)
-        df_protonGen.loc[:, 'GenPtheta'] = getTheta(proGen)
-        df_protonGen.loc[:, 'GenPphi'] = getPhi(proGen)
+        # proGen = [df_protonGen["GenPpx"], df_protonGen["GenPpy"], df_protonGen["GenPpz"]]
+        # df_protonGen.loc[:, 'GenPp'] = mag(proGen)
+        # df_protonGen.loc[:, 'GenPtheta'] = getTheta(proGen)
+        # df_protonGen.loc[:, 'GenPphi'] = getPhi(proGen)
 
         df_z = pd.merge(df_electronGen, df_protonGen, how='inner', on='event')
 
-        df_gammaGen = df_gammaGen[df_gammaGen.index.get_level_values('subentry')==0]
-        gamGen = [df_gammaGen["GenGpx"], df_gammaGen["GenGpy"], df_gammaGen["GenGpz"]]
-        df_gammaGen.loc[:, 'GenGp'] = mag(gamGen)
-        df_gammaGen.loc[:, 'GenGtheta'] = getTheta(gamGen)
-        df_gammaGen.loc[:, 'GenGphi'] = getPhi(gamGen)
+        # df_gammaGen = df_gammaGen[df_gammaGen.index.get_level_values('subentry')==0]
+        # gamGen = [df_gammaGen["GenGpx"], df_gammaGen["GenGpy"], df_gammaGen["GenGpz"]]
+        # df_gammaGen.loc[:, 'GenGp'] = mag(gamGen)
+        # df_gammaGen.loc[:, 'GenGtheta'] = getTheta(gamGen)
+        # df_gammaGen.loc[:, 'GenGphi'] = getPhi(gamGen)
 
         df_z = pd.merge(df_z, df_gammaGen, how='inner', on='event')
 
-        keys = {key:key[3:] for key in df_z.columns}
-        keys["event"] = "event"
-        self.df_epg = df_z.rename(columns=keys)
+        # keys = {key:key[3:] for key in df_z.columns}
+        # keys["event"] = "event"
+        self.df_epg = df_z#.rename(columns=keys)
 
-    def saveDVCSvars(self, correction=None):
-        #set up dvcs variables
-        df_epg = self.df_epg
+    # def saveDVCSvars(self, correction=None):
+    #     #set up dvcs variables
+    #     df_epg = self.df_epg
 
-        ele = [df_epg['Epx'], df_epg['Epy'], df_epg['Epz']]
-        df_epg.loc[:, 'Ep'] = mag(ele)
-        df_epg.loc[:, 'Ee'] = getEnergy(ele, me)
-        df_epg.loc[:, 'Etheta'] = getTheta(ele)
-        df_epg.loc[:, 'Ephi'] = getPhi(ele)
+    #     ele = [df_epg['Epx'], df_epg['Epy'], df_epg['Epz']]
+    #     df_epg.loc[:, 'Ep'] = mag(ele)
+    #     df_epg.loc[:, 'Ee'] = getEnergy(ele, me)
+    #     df_epg.loc[:, 'Etheta'] = getTheta(ele)
+    #     df_epg.loc[:, 'Ephi'] = getPhi(ele)
 
-        pro = [df_epg['Ppx'], df_epg['Ppy'], df_epg['Ppz']]
-        df_epg.loc[:, 'Pp'] = mag(pro)
-        df_epg.loc[:, 'Pe'] = getEnergy(pro, M)
-        df_epg.loc[:, 'Ptheta'] = getTheta(pro)
-        df_epg.loc[:, 'Pphi'] = getPhi(pro)
+    #     pro = [df_epg['Ppx'], df_epg['Ppy'], df_epg['Ppz']]
+    #     df_epg.loc[:, 'Pp'] = mag(pro)
+    #     df_epg.loc[:, 'Pe'] = getEnergy(pro, M)
+    #     df_epg.loc[:, 'Ptheta'] = getTheta(pro)
+    #     df_epg.loc[:, 'Pphi'] = getPhi(pro)
 
-        gam = [df_epg['Gpx'], df_epg['Gpy'], df_epg['Gpz']]
-        df_epg.loc[:, 'Gp'] = mag(gam)
-        df_epg.loc[:, 'Ge'] = getEnergy(gam, 0)
-        df_epg.loc[:, 'Gtheta'] = getTheta(gam)
-        df_epg.loc[:, 'Gphi'] = getPhi(gam)
+    #     gam = [df_epg['Gpx'], df_epg['Gpy'], df_epg['Gpz']]
+    #     df_epg.loc[:, 'Gp'] = mag(gam)
+    #     df_epg.loc[:, 'Ge'] = getEnergy(gam, 0)
+    #     df_epg.loc[:, 'Gtheta'] = getTheta(gam)
+    #     df_epg.loc[:, 'Gphi'] = getPhi(gam)
 
-        Ppt = mag([df_epg['Ppx'], df_epg['Ppy'], 0])
+    #     Ppt = mag([df_epg['Ppx'], df_epg['Ppy'], 0])
 
-        VGS = [-df_epg['Epx'], -df_epg['Epy'], pbeam - df_epg['Epz']]
-        v3l = cross(beam, ele)
-        v3h = cross(pro, VGS)
-        v3g = cross(VGS, gam)
-        VmissG = [-df_epg["Epx"] - df_epg["Ppx"], -df_epg["Epy"] - df_epg["Ppy"],
-                  pbeam - df_epg["Epz"] - df_epg["Ppz"]]
-        VmissP = [-(df_epg["Epx"] + df_epg["Gpx"]), -(df_epg["Epy"] + df_epg["Gpy"]),
-                  -(-pbeam + df_epg["Epz"] + df_epg["Gpz"])]
-        Vmiss = [-(df_epg["Epx"] + df_epg["Ppx"] + df_epg["Gpx"]), -(df_epg["Epy"] + df_epg["Ppy"] + df_epg["Gpy"]),
-                 -(-pbeam + df_epg["Epz"] + df_epg["Ppz"] + df_epg["Gpz"])]
-        costheta = cosTheta(VGS, gam)
+    #     VGS = [-df_epg['Epx'], -df_epg['Epy'], pbeam - df_epg['Epz']]
+    #     v3l = cross(beam, ele)
+    #     v3h = cross(pro, VGS)
+    #     v3g = cross(VGS, gam)
+    #     VmissG = [-df_epg["Epx"] - df_epg["Ppx"], -df_epg["Epy"] - df_epg["Ppy"],
+    #               pbeam - df_epg["Epz"] - df_epg["Ppz"]]
+    #     VmissP = [-(df_epg["Epx"] + df_epg["Gpx"]), -(df_epg["Epy"] + df_epg["Gpy"]),
+    #               -(-pbeam + df_epg["Epz"] + df_epg["Gpz"])]
+    #     Vmiss = [-(df_epg["Epx"] + df_epg["Ppx"] + df_epg["Gpx"]), -(df_epg["Epy"] + df_epg["Ppy"] + df_epg["Gpy"]),
+    #              -(-pbeam + df_epg["Epz"] + df_epg["Ppz"] + df_epg["Gpz"])]
+    #     costheta = cosTheta(VGS, gam)
 
-        df_epg.loc[:, 'Mpx'], df_epg.loc[:, 'Mpy'], df_epg.loc[:, 'Mpz'] = Vmiss
+    #     # df_epg.loc[:, 'Mpx'], df_epg.loc[:, 'Mpy'], df_epg.loc[:, 'Mpz'] = Vmiss
 
-        # binning kinematics
-        df_epg.loc[:,'Q2'] = -((ebeam - df_epg['Ee'])**2 - mag2(VGS))
-        df_epg.loc[:,'nu'] = (ebeam - df_epg['Ee'])
-        df_epg.loc[:,'y'] = df_epg['nu']/ebeam
-        df_epg.loc[:,'xB'] = df_epg['Q2'] / 2.0 / M / df_epg['nu']
-        df_epg.loc[:,'t1'] = 2 * M * (df_epg['Pe'] - M)
-        df_epg.loc[:,'t2'] = (M * df_epg['Q2'] + 2 * M * df_epg['nu'] * (df_epg['nu'] - np.sqrt(df_epg['nu'] * df_epg['nu'] + df_epg['Q2']) * costheta))\
-        / (M + df_epg['nu'] - np.sqrt(df_epg['nu'] * df_epg['nu'] + df_epg['Q2']) * costheta)
-        df_epg.loc[:,'W'] = np.sqrt(np.maximum(0, (ebeam + M - df_epg['Ee'])**2 - mag2(VGS)))
+    #     # binning kinematics
+    #     df_epg.loc[:,'Q2'] = -((ebeam - df_epg['Ee'])**2 - mag2(VGS))
+    #     df_epg.loc[:,'nu'] = (ebeam - df_epg['Ee'])
+    #     df_epg.loc[:,'y'] = df_epg['nu']/ebeam
+    #     df_epg.loc[:,'xB'] = df_epg['Q2'] / 2.0 / M / df_epg['nu']
+    #     df_epg.loc[:,'t1'] = 2 * M * (df_epg['Pe'] - M)
+    #     df_epg.loc[:,'t2'] = (M * df_epg['Q2'] + 2 * M * df_epg['nu'] * (df_epg['nu'] - np.sqrt(df_epg['nu'] * df_epg['nu'] + df_epg['Q2']) * costheta))\
+    #     / (M + df_epg['nu'] - np.sqrt(df_epg['nu'] * df_epg['nu'] + df_epg['Q2']) * costheta)
+    #     df_epg.loc[:,'W'] = np.sqrt(np.maximum(0, (ebeam + M - df_epg['Ee'])**2 - mag2(VGS)))
 
-        # trento angles
-        df_epg.loc[:,'phi1'] = angle(v3l, v3h)
-        df_epg.loc[:,'phi1'] = np.where(dot(v3l, pro) > 0, 360.0 -
-                                  df_epg['phi1'], df_epg['phi1'])
-        df_epg.loc[:,'phi2'] = angle(v3l, v3g)
-        df_epg.loc[:,'phi2'] = np.where(dot(v3l, gam) <
-                                  0, 360.0 - df_epg['phi2'], df_epg['phi2'])
+    #     # trento angles
+    #     df_epg.loc[:,'phi1'] = angle(v3l, v3h)
+    #     df_epg.loc[:,'phi1'] = np.where(dot(v3l, pro) > 0, 360.0 -
+    #                               df_epg['phi1'], df_epg['phi1'])
+    #     df_epg.loc[:,'phi2'] = angle(v3l, v3g)
+    #     df_epg.loc[:,'phi2'] = np.where(dot(v3l, gam) <
+    #                               0, 360.0 - df_epg['phi2'], df_epg['phi2'])
 
-        # exclusivity variables
-        df_epg.loc[:,'MM2_epg'] = (-M - ebeam + df_epg["Ee"] +
-                             df_epg["Pe"] + df_epg["Ge"])**2 - mag2(Vmiss)
-        df_epg.loc[:,'ME_epg'] = (M + ebeam - df_epg["Ee"] - df_epg["Pe"] - df_epg["Ge"])
-        df_epg.loc[:,'MM2_ep'] = (-M - ebeam + df_epg["Ee"] + df_epg["Pe"])**2 - mag2(VmissG)
-        df_epg.loc[:,'MM2_eg'] = (-M - ebeam + df_epg["Ee"] + df_epg["Ge"])**2 - mag2(VmissP)
-        df_epg.loc[:,'MPt'] = np.sqrt((df_epg["Epx"] + df_epg["Ppx"] + df_epg["Gpx"])**2 +
-                                (df_epg["Epy"] + df_epg["Ppy"] + df_epg["Gpy"])**2)
-        df_epg.loc[:,'coneAngle'] = angle(ele, gam)
-        df_epg.loc[:,'reconGam'] = angle(gam, VmissG)
-        df_epg.loc[:,'coplanarity'] = angle(v3h, v3g)
-        self.df_epg = df_epg
+    #     # # exclusivity variables
+    #     # df_epg.loc[:,'MM2_epg'] = (-M - ebeam + df_epg["Ee"] +
+    #     #                      df_epg["Pe"] + df_epg["Ge"])**2 - mag2(Vmiss)
+    #     # df_epg.loc[:,'ME_epg'] = (M + ebeam - df_epg["Ee"] - df_epg["Pe"] - df_epg["Ge"])
+    #     # df_epg.loc[:,'MM2_ep'] = (-M - ebeam + df_epg["Ee"] + df_epg["Pe"])**2 - mag2(VmissG)
+    #     # df_epg.loc[:,'MM2_eg'] = (-M - ebeam + df_epg["Ee"] + df_epg["Ge"])**2 - mag2(VmissP)
+    #     # df_epg.loc[:,'MPt'] = np.sqrt((df_epg["Epx"] + df_epg["Ppx"] + df_epg["Gpx"])**2 +
+    #     #                         (df_epg["Epy"] + df_epg["Ppy"] + df_epg["Gpy"])**2)
+    #     # df_epg.loc[:,'coneAngle'] = angle(ele, gam)
+    #     # df_epg.loc[:,'reconGam'] = angle(gam, VmissG)
+    #     # df_epg.loc[:,'coplanarity'] = angle(v3h, v3g)
+    #     self.df_epg = df_epg
 
     def saveRaw(self):
         self.df = self.df_epg
