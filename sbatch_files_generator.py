80a103abfd1c0abe7d29e2c7d089c1cbb2e60b65
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-05-14 16:55:39 -0500

    Clarify the loop

--- sbatch_files_generator.py
+++ sbatch_files_generator.py
@@ -1,6 +1,7 @@
 from glob import glob
 import argparse
 import subprocess
+import itertools
 
 
 parser = argparse.ArgumentParser(description="Get args",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -16,14 +17,15 @@ configs = {
   "pi0/outb": [6069]
 }
 
-default_dir = "/volaite/clas12/sangbaek/jan2023/sbatch_files"
+default_dir = "/volatile/clas12/sangbaek/jan2023/sbatch_files"
 
 #step 1.
 #merge simulations.
 #filter files into a hipo files that have
 
 mode        = "collecting_dsts"
-for key, run in configs.items():
-  for i in [1,2,3,4]:
-    subprocess.run(['cp','{}/{}/.run_{}'.format(default_dir, mode, i), '{}/{}/{}_{}'.format(default_dir, mode, run, i)])
-    subprocess.run(['sed', '-i', 's/run/{}/g'.format(run), '{}/{}/{}_{}'.format(default_dir, mode, run, i)])
+for config, i in (itertools.product(configs.items(), [1,2,3,4])):
+  mode, runs = config
+  for run in runs:
+    subprocess.run(['cp','{}/{}/.run_{}'.format(default_dir, mode, i), '{}/{}/{}_{}'.format(default_dir, mode, runs, i)])
+    subprocess.run(['sed', '-i', 's/run/{}/g'.format(run), '{}/{}/{}_{}'.format(default_dir, mode, runs, i)])
