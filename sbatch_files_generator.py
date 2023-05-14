0b48396bdfd13d1661e8fbb727bebf7d363b33e1
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-05-14 18:19:29 -0500

    Save exp case

--- sbatch_files_generator.py
+++ sbatch_files_generator.py
@@ -7,6 +7,7 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description="Get args",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-s","--step", help="what to do?", default = 0, type = int)
+parser.add_argument("-d","--data", help="sim or exp", default = 'sim', type = str)
 args   = parser.parse_args()
 
 configs = {
@@ -20,36 +21,54 @@ configs = {
 
 default_dir    = "/volatile/clas12/sangbaek/jan2023/sbatch_files"
 step           = args.step
-entries        = np.linspace(0, 19, 20, dtype = int)
-this_step_dirs = {1: "collecting_dsts/sim/", 3: "filtering/", 4: "conversion/", 5: "final/"}
+this_step_dirs = {1: "collecting_dsts/sim/", 3: "filtering/", 4: "conversion/", 5: "final/", 6: "Gen/", 7: "Gen2/"}
 
-#step 1.
-#merge simulations.
-#filter files into a hipo files that have
-if step == 1:
-  this_step_dir = this_step_dirs[step]
-  for config, entry in (itertools.product(configs.items(), [1,2,3,4])):
-    mode, runs = config
-    for run in runs:
-      subprocess.run(['cp','{}/{}/.run_{}'.format(default_dir, this_step_dir, entry), '{}/{}/{}_{}'.format(default_dir, this_step_dir, run, entry)])
-      subprocess.run(['sed', '-i', 's/run/{}/g'.format(run), '{}/{}/{}_{}'.format(default_dir, this_step_dir, run, entry)])
+if args.data == "sim":
+  entries        = np.linspace(0, 19, 20, dtype = int)
 
-#step 2.
-#cp the simulation files to the temp directory
-elif step == 2:
-  for config, entry in (itertools.product(configs.items(), entries)):
-    mode, runs = config
-    for run in runs:
-      subprocess.run(['mv','/volatile/clas12/sangbaek/jan2023/to_be_filtered/{}_{}.hipo'.format(run, entry), '/volatile/clas12/sangbaek/regular_backup/temp/{}/{}_{}.hipo'.format(mode, run, entry)])
+  #step 1.
+  #merge simulations.
+  if step == 1:
+    this_step_dir = this_step_dirs[step]
+    for config, entry in (itertools.product(configs.items(), [1,2,3,4])):
+      mode, runs = config
+      for run in runs:
+        subprocess.run(['cp','{}/{}/.run_{}'.format(default_dir, this_step_dir, entry), '{}/{}/{}_{}'.format(default_dir, this_step_dir, run, entry)])
+        subprocess.run(['sed', '-i', 's/run/{}/g'.format(run), '{}/{}/{}_{}'.format(default_dir, this_step_dir, run, entry)])
+
+  #step 2.
+  #cp the simulation files to the temp directory
+  elif step == 2:
+    for config, entry in (itertools.product(configs.items(), entries)):
+      mode, runs = config
+      for run in runs:
+        subprocess.run(['mv','/volatile/clas12/sangbaek/jan2023/to_be_filtered/{}_{}.hipo'.format(run, entry), '/volatile/clas12/sangbaek/regular_backup/temp/{}/{}_{}.hipo'.format(mode, run, entry)])
 
 
-#step 3: filtering to hipo files that have epg, 4: convert to root files, 5: convert to pkl files.
-#convert that into root files
-else:
-  this_step_dir = this_step_dirs[step]
-  for config, entry in (itertools.product(configs.items(), entries)):
-    mode, runs = config
-    for run in runs:
-      subprocess.run(['cp','{}/{}/{}/.run_0'.format(default_dir, this_step_dir, mode, entry), '{}/{}/{}/{}_{}'.format(default_dir, this_step_dir, mode, run, entry)])
-      subprocess.run(['sed', '-i', 's/run/{}/g;s/_0/_{}/g'.format(run, entry), '{}/{}/{}/{}_{}'.format(default_dir, this_step_dir, mode, run, entry)])
+  #step 3, 4, 5: filtering to hipo files that have epg, 4: convert to root files, 5: convert to pkl files.
+  #convert that into root files
+  else:
+    this_step_dir = this_step_dirs[step]
+    for config, entry in (itertools.product(configs.items(), entries)):
+      mode, runs = config
+      for run in runs:
+        subprocess.run(['cp','{}/{}/{}/.run_0'.format(default_dir, this_step_dir, mode, entry), '{}/{}/{}/{}_{}'.format(default_dir, this_step_dir, mode, run, entry)])
+        subprocess.run(['sed', '-i', 's/run/{}/g;s/_0/_{}/g'.format(run, entry), '{}/{}/{}/{}_{}'.format(default_dir, this_step_dir, mode, run, entry)])
+
+elif args.data == "exp":
+  #step 3
+  #filter files into a hipo files that have epg
+  if step == 3:
+    #DVCS wagon inb
+    file_locations = {
+      "epg/inb": "/cache/clas12/rg-a/production/recon/fall2018/torus-1/pass1/v1/dst/train/DVCSWagon/",
+      "epg/outb": "/cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v1/dst/train/DVCSWagon/",
+      "epgg/inb": "/cache/clas12/rg-a/production/recon/fall2018/torus-1/pass1/v1/dst/train/DVPi0P/",
+      "epgg/outb": "/cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v1/dst/train/DVPi0P/",
+    }
+    for mode in file_locations:
+      files = glob("{}/*".format(file_locations[mode]))
+      for file in files:
+        run = file[0].split('.hipo')[-2].split('DVCSWagon_00')[-1]
+        subprocess.run(['cp','{}/{}/exp/{}/.run'.format(default_dir, this_step_dir, mode), '{}/{}/exp/{}/.{}'.format(default_dir, this_step_dir, mode, run)])
 
