b733dc4c0d8956307b9ea7f34ff44f58fd25f9e1
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-05-15 23:45:34 -0500

    Save a script to assign qadb list

---
+++ assign_qadb.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import argparse
+from copy import copy
+
+parser = argparse.ArgumentParser(description="Get args",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-p","--polarity", help="polarity: inb or outb", default = "inb", type = str)
+parser.add_argument("-r","--run", help="run number", default = 5032, type = int)
+args  = parser.parse_args()
+
+run      = args.run
+polarity = args.polarity
+
+
+qaTree     = pd.read_json("/volatile/clas12/sangbaek/clasqaDB/qadb/qa.rga_{}ending/qaTree.json".format(polarity)).T
+chargeTree = pd.read_json("/volatile/clas12/sangbaek/clasqaDB/qadb/qa.rga_{}ending/chargeTree.json".format(polarity)).T
+filename   = "/volatile/clas12/sangbaek/jan2023/convPkl/exp/epg/{}/{}.pkl".format(polarity, run)
+file       = pd.read_pickle(filename)
+
+for i in range(339):
+  filenum    = 5*i
+  qadb       = qaTree.loc[qaTree.index == run, filenum]
+  if np.isnan(qadb):
+    break    
+  chargedb   = chargeTree.loc[qaTree.index == run, filenum]
+  evnumMin   = qadb.values[0]['evnumMin']
+  evnumMax   = qadb.values[0]['evnumMax']
+  defect     = qadb.values[0]['defect']
+  chargemin  = chargedb.values[0]['fcChargeMin']
+  chargemax  = chargedb.values[0]['fcChargeMax']
+  file.loc[(file.EventNum>=evnumMin) & (file.EventNum<=evnumMax), "filenum"] = filenum
+  file.loc[(file.EventNum>=evnumMin) & (file.EventNum<=evnumMax), "defect"]  = defect
+  file.loc[(file.EventNum>=evnumMin) & (file.EventNum<=evnumMax), "chargemin"] = chargemin
+  file.loc[(file.EventNum>=evnumMin) & (file.EventNum<=evnumMax), "chargemax"] = chargemax
+
+file.to_pickle(filename)
