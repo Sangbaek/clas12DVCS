d08601cb9251c40162f97a991177e3597b050c64
Sangbaek Lee <sangbaek.lee@anl.gov>
2023-05-17 10:38:36 -0500

    Prepare for the epgg pkl files

--- assign_qadb.py
+++ assign_qadb.py
@@ -6,15 +6,19 @@ from copy import copy
 parser = argparse.ArgumentParser(description="Get args",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-p","--polarity", help="polarity: inb or outb", default = "inb", type = str)
 parser.add_argument("-r","--run", help="run number", default = 5032, type = int)
+parser.add_argument("-pi0", '--pi0', action = 'store_true')
 args  = parser.parse_args()
 
 run      = args.run
 polarity = args.polarity
 
+mode = "epg"
+if args.pi0:
+  mode = "epgg"
 
 qaTree     = pd.read_json("/volatile/clas12/sangbaek/clasqaDB/qadb/qa.rga_{}ending/qaTree.json".format(polarity)).T
 chargeTree = pd.read_json("/volatile/clas12/sangbaek/clasqaDB/qadb/qa.rga_{}ending/chargeTree.json".format(polarity)).T
-filename   = "/volatile/clas12/sangbaek/jan2023/convPkl/exp/epg/{}/{}.pkl".format(polarity, run)
+filename   = "/volatile/clas12/sangbaek/jan2023/convPkl/exp/{}/{}/{}.pkl".format(mode, polarity, run)
 file       = pd.read_pickle(filename)
 
 for i in range(339):
