183197c55afeea70e903a3f770a56ebd258d3a0d
Sangbaek Lee <sangbaek.lee@anl.gov>
2024-04-09 16:14:50 -0500

    Replace the dead wire fiducial cut with the inclusive one

--- utils/fiducial.py
+++ utils/fiducial.py
@@ -4,6 +4,9 @@ from utils.physics import *
 def electronFiducial(df_electronRec, mc = False, fidlevel = 'mid'):
 	# following inclusive analysis note
 	df_electronRec.loc[:, "EFid"] = 1
+	# D. nphe cut
+	df_electronRec.loc[df_electronRec.Enphe <= min_nphe, "EFid"] = 0
+
 	# E. vz cut
 	df_electronRec.loc[df_electronRec.Evz < -8, "EFid"] = 0
 	df_electronRec.loc[df_electronRec.Evz >  2, "EFid"] = 0
@@ -447,6 +450,100 @@ def electronFiducial_legacy(df_electronRec, pol = "inbending", mc = False, fidle
 	return df_electronRec.loc[df_electronRec.EFid==1, :]
 
 def gammaFiducial(df_gammaRec):
+	df_gammaRec.loc[:, "GFid"] = 1
+	#passGammaPCALFiducialCut
+	df_gammaRec.loc[(df_gammaRec.GcalV1 <= g_min_v) & (df_gammaRec.Gsector<7), "GFid"] = 0
+	df_gammaRec.loc[(df_gammaRec.GcalW1 <= g_min_w) & (df_gammaRec.Gsector<7), "GFid"] = 0
+	#passGammaBetaCut
+	df_gammaRec.loc[df_gammaRec.Gbeta <= min_Gbeta, "GFid"] = 0
+	df_gammaRec.loc[df_gammaRec.Gbeta >= max_Gbeta, "GFid"] = 0
+
+	df_gammaRec.loc[df_gammaRec.Gsector<7, "GFid"] = 0
+
+	#apply photon fiducial cuts
+	sector_cond = [df_gammaRec.Gsector ==1, df_gammaRec.Gsector ==2, df_gammaRec.Gsector ==3, df_gammaRec.Gsector ==4, df_gammaRec.Gsector ==5, df_gammaRec.Gsector ==6]
+	psplit = np.select(sector_cond, [87, 82, 85, 77, 78, 82])
+	tleft = np.select(sector_cond, [58.7356, 62.8204, 62.2296, 53.7756, 58.2888, 54.5822])
+	tright = np.select(sector_cond, [58.7477, 51.2589, 59.2357, 56.2415, 60.8219, 49.8914])
+	sleft = np.select(sector_cond, [0.582053, 0.544976, 0.549788, 0.56899, 0.56414, 0.57343])
+	sright = np.select(sector_cond, [-0.591876, -0.562926, -0.562246, -0.563726, -0.568902, -0.550729])
+	rleft = np.select(sector_cond, [64.9348, 64.7541, 67.832, 55.9324, 55.9225, 60.0997])
+	rright = np.select(sector_cond, [65.424, 54.6992, 63.6628, 57.8931, 56.5367, 56.4641])
+	qleft = np.select(sector_cond, [0.745578, 0.606081, 0.729202, 0.627239, 0.503674, 0.717899])
+	qright = np.select(sector_cond, [-0.775022, -0.633863, -0.678901, -0.612458, -0.455319, -0.692481])
+	#first condition
+	ang = np.radians((df_gammaRec.loc[df_gammaRec.Gsector<7, "Gsector"]-1) * 60)
+	GcX_rot = df_gammaRec.loc[df_gammaRec.Gsector<7, "GcY"] * np.sin(ang) + df_gammaRec.loc[df_gammaRec.Gsector<7, "GcX"] * np.cos(ang)
+	GcY_rot = df_gammaRec.loc[df_gammaRec.Gsector<7, "GcY"] * np.cos(ang) - df_gammaRec.loc[df_gammaRec.Gsector<7, "GcX"] * np.sin(ang)
+
+	df_gammaRec.loc[df_gammaRec.Gsector<7, "GcX"] = GcX_rot
+	df_gammaRec.loc[df_gammaRec.Gsector<7, "GcY"] = GcY_rot
+
+	cond1_1 = df_gammaRec.GcX >= psplit
+	cond1_2 = df_gammaRec.GcY < sleft * (df_gammaRec.GcX - tleft)
+	cond1_3 = df_gammaRec.GcY > sright * (df_gammaRec.GcX - tright)
+	cond1_4 = df_gammaRec.Gsector < 7
+	cond1 = cond1_1 & cond1_2 & cond1_3 & cond1_4
+	df_gammaRec.loc[cond1, "GFid"] = 1
+	#second condition else if the first
+	# cond2_0 = df_gammaRec.GFid == 0 # not necessary, because cond2_1 rules out the first (S. Lee)
+	cond2_1 = df_gammaRec.GcX < psplit
+	cond2_2 = df_gammaRec.GcY < qleft * (df_gammaRec.GcX - rleft)
+	cond2_3 = df_gammaRec.GcY > qright * (df_gammaRec.GcX - rright)
+	cond2_4 = df_gammaRec.Gsector < 7
+	cond2 = cond2_1 & cond2_2 & cond2_3 & cond2_4
+	df_gammaRec.loc[cond2, "GFid"] = 1
+    #photon FD fiducial cuts by F.X. Girod
+
+	#FT fiducial cuts
+	circleCenterX1 = -8.419
+	circleCenterY1 = 9.889
+	circleRadius1 = 1.6
+
+	circleCenterX2 = -9.89
+	circleCenterY2 = -5.327
+	circleRadius2 = 1.6
+
+	circleCenterX3 = -6.15
+	circleCenterY3 = -13
+	circleRadius3 = 2.3
+
+	circleCenterX4 = 3.7
+	circleCenterY4 = -6.5
+	circleRadius4 = 2
+
+	circle1 = (df_gammaRec.GcX - circleCenterX1)**2 + (df_gammaRec.GcY - circleCenterY1)**2 < circleRadius1**2
+	circle2 = (df_gammaRec.GcX - circleCenterX2)**2 + (df_gammaRec.GcY - circleCenterY2)**2 < circleRadius2**2
+	circle3 = (df_gammaRec.GcX - circleCenterX3)**2 + (df_gammaRec.GcY - circleCenterY3)**2 < circleRadius3**2
+	circle4 = (df_gammaRec.GcX - circleCenterX4)**2 + (df_gammaRec.GcY - circleCenterY4)**2 < circleRadius4**2
+
+	df_gammaRec.loc[(df_gammaRec.Gsector > 7) & circle1, "GFid"] = 0
+	df_gammaRec.loc[(df_gammaRec.Gsector > 7) & circle2, "GFid"] = 0
+	df_gammaRec.loc[(df_gammaRec.Gsector > 7) & circle3, "GFid"] = 0
+	df_gammaRec.loc[(df_gammaRec.Gsector > 7) & circle4, "GFid"] = 0
+
+	#Table XII.
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 1) & (df_gammaRec.GcalHy1 <= 0.56575  * df_gammaRec.GcalHx1 -92        + 0.25) & (df_gammaRec.GcalHy1 >= 0.56575 * df_gammaRec.GcalHx1 -94.4         - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 1) & (df_gammaRec.GcalHy1 <= 0.56575  * df_gammaRec.GcalHx1 -101.1     + 0.25) & (df_gammaRec.GcalHy1 >= 0.56575 * df_gammaRec.GcalHx1 -103.5        - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 1) & (df_gammaRec.GcalHy1 <= 0.56575  * df_gammaRec.GcalHx1 -219       + 0.25) & (df_gammaRec.GcalHy1 >= 0.56575 * df_gammaRec.GcalHx1 -221.4        - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 1) & (df_gammaRec.GcalHy1 <= 0.56575  * df_gammaRec.GcalHx1 -227       + 0.25) & (df_gammaRec.GcalHy1 >= 0.56575 * df_gammaRec.GcalHx1 -229.4        - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 2) & (df_gammaRec.GcalHy1 <= 0.5897   * df_gammaRec.GcalHx1 +120.7937  + 0.25) & (df_gammaRec.GcalHy1 >= 0.5913  * df_gammaRec.GcalHx1 +114.3872     - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 2) & (df_gammaRec.GcalHy1 <= 107.2766 * df_gammaRec.GcalHx1 -10602.9779+ 0.25) & (df_gammaRec.GcalHy1 >= 98.9667 * df_gammaRec.GcalHx1 -10262.0167   - 0.25), "GFid"] = 0
+ 	df_gammaRec.loc[ (df_gammaRec.Gsector == 3) & (df_gammaRec.GcalHx1 <= -302.38) & (df_gammaRec.GcalHx1 >= -313.71), "GFid"] = 0
+ 	df_gammaRec.loc[ (df_gammaRec.Gsector == 4) & (df_gammaRec.GcalHx1 <= -122.5 ) & (df_gammaRec.GcalHx1 >= -127.5 ), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 4) & (df_gammaRec.GcalHy1 <= -0.568   * df_gammaRec.GcalHx1 -232.8     + 0.25) & (df_gammaRec.GcalHy1 >= -0.568  * df_gammaRec.GcalHx1 -236.3        - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 5) & (df_gammaRec.GcalHy1 <= 98.0644  * df_gammaRec.GcalHx1 +5825.4023 + 0.25) & (df_gammaRec.GcalHy1 >= 99.9337 * df_gammaRec.GcalHx1 +5098.3456    - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 6) & (df_gammaRec.GcalHy1 <= 0.4547   * df_gammaRec.GcalHx1 -275.9317  + 0.25) & (df_gammaRec.GcalHy1 >= 0.4547  * df_gammaRec.GcalHx1 -285.9317     - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 6) & (df_gammaRec.GcalHy1 <= 0.591377  * df_gammaRec.GcalHx1 -185      + 0.25) & (df_gammaRec.GcalHy1 >= 0.591377* df_gammaRec.GcalHx1 -187          - 0.25), "GFid"] = 0
+	df_gammaRec.loc[ (df_gammaRec.Gsector == 6) & (df_gammaRec.GcalHy1 <= 0.591377  * df_gammaRec.GcalHx1 -193.3    + 0.25) & (df_gammaRec.GcalHy1 >= 0.591377* df_gammaRec.GcalHx1 -195.5        - 0.25), "GFid"] = 0
+
+   	df_gammaRec.loc[ (df_gammaRec.Gsector == 5) & (df_gammaRec.GcalHy3 <= -0.5841  * df_gammaRec.GcalHx3 -252.11    + 0.25) & (df_gammaRec.GcalHy3 >= -0.5775 * df_gammaRec.GcalHx3 -263.2072    - 0.25), "GFid"] = 0
+
+	return df_gammaRec.loc[df_gammaRec.GFid==1, :]
+
+
+
+def gammaFiducialLegacy(df_gammaRec):
 	df_gammaRec.loc[:, "GFid"] = 1
 	#passGammaPCALFiducialCut
 	df_gammaRec.loc[(df_gammaRec.GcalV1 <= g_min_v) & (df_gammaRec.Gsector<7), "GFid"] = 0
