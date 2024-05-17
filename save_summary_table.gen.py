52b5902a8cc6e8a1746f1851d54eb7d376cad49e
Sangbaek <sangbaek.lee@anl.gov>
2024-05-17 16:37:06 -0400

    Save the summary table for generator

--- save_summary_table.gen.py
+++ save_summary_table.gen.py
@@ -160,29 +160,29 @@ def main(mode):
 						df_this_bin  = df.loc[df.phi_binnum_gen == phi_binnum_gen]
 						directory    = "{}/fall2018_inb{}".format(model, "3")
 						n_entry      = len(df_this_bin)
-					if n_entry:
-						weight_sum   = np.sum(df_this_bin.GenWeight)
-						weight_avg  = np.mean(df_this_bin.GenWeight)
-						xB_avg      = np.sum(df_this_bin.GenWeight * df_this_bin.GenxB)/weight_sum
-						Q2_avg      = np.sum(df_this_bin.GenWeight * df_this_bin.GenQ2)/weight_sum
-						t_avg       = np.sum(df_this_bin.GenWeight * df_this_bin.Gent)/weight_sum
-						phi_avg     = np.sum(df_this_bin.GenWeight * df_this_bin.Genphi)/weight_sum
-						df_this_bin.loc[:, "bin_volume_gen"] = bin_volume[integrated_binnum_gen]/24.
-						df_this_bin.loc[:, "weights"] = df_this_bin.GenWeight * df_this_bin.loc[:, "bin_volume_gen"] * luminosity_inb / len(df_this_bin)
-						n_generated = np.sum(df_this_bin.weights)
-					else:
-						weight_sum  = 0#np.sum(df_this_bin.GenWeight)
-						weight_avg  = 0#np.mean(df_this_bin.GenWeight)
-						xB_avg      = 0#np.sum(df_this_bin.GenWeight * df_this_bin.GenxB)/weight_sum
-						Q2_avg      = 0#np.sum(df_this_bin.GenWeight * df_this_bin.GenQ2)/weight_sum
-						t_avg       = 0#np.sum(df_this_bin.GenWeight * df_this_bin.Gent)/weight_sum
-						phi_avg     = 0#np.sum(df_this_bin.GenWeight * df_this_bin.Genphi)/weight_sum
-						n_generated = 0#np.sum(df_this_bin.weights)
-					this_row     = pd.DataFrame([{"integrated_binnum_gen": integrated_binnum_gen, "phi_binnum_gen": phi_binnum_gen, "directory": directory, "n_entry": n_entry,
-						"bin_volume_gen": bin_volume[integrated_binnum_gen]/24., "n_generated": n_generated, "weight_sum": weight_sum, "weight_avg": weight_avg, "xB_avg": xB_avg,
-						"Q2_avg": Q2_avg, "t_avg": t_avg, "phi_avg": phi_avg}])
-					print(this_row)
-					df_summary   = pd.concat([df_summary, this_row])
+						if n_entry:
+							weight_sum   = np.sum(df_this_bin.GenWeight)
+							weight_avg  = np.mean(df_this_bin.GenWeight)
+							xB_avg      = np.sum(df_this_bin.GenWeight * df_this_bin.GenxB)/weight_sum
+							Q2_avg      = np.sum(df_this_bin.GenWeight * df_this_bin.GenQ2)/weight_sum
+							t_avg       = np.sum(df_this_bin.GenWeight * df_this_bin.Gent)/weight_sum
+							phi_avg     = np.sum(df_this_bin.GenWeight * df_this_bin.Genphi)/weight_sum
+							df_this_bin.loc[:, "bin_volume_gen"] = bin_volume[integrated_binnum_gen]/24.
+							df_this_bin.loc[:, "weights"] = df_this_bin.GenWeight * df_this_bin.loc[:, "bin_volume_gen"] * luminosity_inb / len(df_this_bin)
+							n_generated = np.sum(df_this_bin.weights)
+						else:
+							weight_sum  = 0#np.sum(df_this_bin.GenWeight)
+							weight_avg  = 0#np.mean(df_this_bin.GenWeight)
+							xB_avg      = 0#np.sum(df_this_bin.GenWeight * df_this_bin.GenxB)/weight_sum
+							Q2_avg      = 0#np.sum(df_this_bin.GenWeight * df_this_bin.GenQ2)/weight_sum
+							t_avg       = 0#np.sum(df_this_bin.GenWeight * df_this_bin.Gent)/weight_sum
+							phi_avg     = 0#np.sum(df_this_bin.GenWeight * df_this_bin.Genphi)/weight_sum
+							n_generated = 0#np.sum(df_this_bin.weights)
+						this_row     = pd.DataFrame([{"integrated_binnum_gen": integrated_binnum_gen, "phi_binnum_gen": phi_binnum_gen, "directory": directory, "n_entry": n_entry,
+							"bin_volume_gen": bin_volume[integrated_binnum_gen]/24., "n_generated": n_generated, "weight_sum": weight_sum, "weight_avg": weight_avg, "xB_avg": xB_avg,
+							"Q2_avg": Q2_avg, "t_avg": t_avg, "phi_avg": phi_avg}])
+						print(this_row)
+						df_summary   = pd.concat([df_summary, this_row])
 
 			if model == "dvcs_km15":
 				string_suffix = "3_45nA"
