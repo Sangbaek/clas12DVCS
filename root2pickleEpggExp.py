26ced664c0d58af707edb7467912b1d7ade64c26
Sangbaek Lee <sangbaek.lee@anl.gov>
2024-05-19 15:54:39 -0500

    Prevent indentation error

--- root2pickleEpggExp.py
+++ root2pickleEpggExp.py
@@ -359,6 +359,7 @@ class root2pickle():
             # df_protonRec = df_protonRec.drop(["PDc1Hitx", "PDc1Hity", "PDc1Hitz", "PDc1theta", "PCvt12Hitx", "PCvt12Hity", "PCvt12Hitz"], axis = 1)
             # df_gammaRec = df_gammaRec.drop(["GcX", "GcY"], axis = 1)
             # df_gg = df_gg.drop(["GcX", "GcY", "GcX2", "GcY2"], axis = 1)
+            pass
         
         df_ep = pd.merge(df_electronRec, df_protonRec, how='inner', on='event')
         if logistics:
